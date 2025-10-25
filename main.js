80ea97bc8e493e232b9694600e1abb7d6e93893c
nimrodsegev <nimrod.segev@mail.huji.ac.il>
2025-10-25 19:00:34 +0300

    Final version with all updates

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+// Show popup quote temporarily
 document.getElementById("showQuoteBtn").addEventListener("click", () => {
   const quoteBox = document.getElementById("quoteBox");
   quoteBox.classList.add("show");
 
-  // Automatically hide after 3 seconds
+  // Hide after 3 seconds
   setTimeout(() => {
     quoteBox.classList.remove("show");
   }, 3000);
 });
+
+// Toggle hidden quote visibility
 const toggleBtn = document.getElementById("toggleBtn");
 const highlightText = document.getElementById("highlightText");
 
